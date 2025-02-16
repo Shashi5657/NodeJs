ea747140fce12e2cf5baf70dfbde7da2ca57c9f2
shashi5657 <sangepushashidhar@gmail.com>
2025-02-16 22:42:15 +0530

    node express

--- subscription-tracker/app.js
+++ subscription-tracker/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { PORT } from "./config/env.js";
+import { PORT, MONGO_URI } from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
