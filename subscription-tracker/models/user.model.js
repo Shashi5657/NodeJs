fcf628f2e2b336bc13eb3ec6dfa170c753296d4c
shashi5657 <sangepushashidhar@gmail.com>
2025-02-18 16:46:05 +0530

    adding database

---
+++ subscription-tracker/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User Name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: 6,
+    },
+  },
+  { Timestamp: true }
+);
+
+const User = Mongoose.model("User", userSchema);
+
+export default User
