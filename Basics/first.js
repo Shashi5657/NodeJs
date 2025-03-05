85adc78261187f8d8492349d91d635fa57559918
shashi5657 <sangepushashidhar@gmail.com>
2025-03-05 21:02:07 +0530

    routing

--- Basics/first.js
+++ Basics/first.js
@@ -1,8 +1,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  process.exit();
+  // console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>First Node Page</title></head>");
+
+  if (req.url === "/") {
+    res.write("<body><h1>Home Page</h1></body>");
+  } else if (req.url.toLowerCase() === "/about") {
+    res.write("<body><h1>About Page</h1></body>");
+  }
+  res.write("<body><h3>First Node Page</h3></body>");
+  res.write("</head>");
+  // process.exit();
 });
 
 server.listen(3000, () => {
