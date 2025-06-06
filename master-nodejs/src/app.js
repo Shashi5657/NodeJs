81f1ccc53a2e5dd935afd7d32a66fee7d8858945
shashi5657 <sangepushashidhar@gmail.com>
2025-06-06 19:37:19 +0530

    lefthook & biome is working properly

--- master-nodejs/src/app.js
+++ master-nodejs/src/app.js
@@ -1,10 +1,8 @@
-matrix(
-  1, 0, 0,
-  0, 1, 0,
-  0, 0, 1
-)
-console.log('testing the format & pre commit')
-console.log('another line to test prettier')
+let matrix;
+
+matrix(1, 0, 0, 0, 1, 0, 0, 0, 1);
+console.warn("testing the format & pre commit");
+console.info("another line to test prettier");
 // prettier-ignore
 matrix(
   1, 0, 0,
