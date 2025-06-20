5f786e6166f982046aa34d6e92a3d89b7198ca26
shashi5657 <sangepushashidhar@gmail.com>
2025-06-20 12:54:45 +0530

    exprfess

--- node/index.js
+++ node/index.js
@@ -1,9 +1,22 @@
-import e from "express";
+import express from "express";
+import users from "./MOCK_DATA.json" assert { type: "json" };
 
-const app = e();
+const app = express();
 
 app.get("/", (req, res) => {
   res.send(`Hello ${req.query.name}, This is Home Page`);
+});
+
+app.get("/api/users", (req, res) => {
+  const html = `
+    <ul>
+      ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>`;
+  res.send(html);
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
 });
 
 app.listen(8000, () => console.log("Server started on PORT 8000"));
