47dc27d552f36cab666916dd4faa51722d2132f8
shashi5657 <sangepushashidhar@gmail.com>
2025-07-09 15:13:12 +0530

    initiation of blog app

--- blog-app/index.js
+++ blog-app/index.js
@@ -1,10 +1,15 @@
 import e from "express";
 import path from "node:path";
+import do
 
 const app = e();
 
 app.set("view engine", "ejs");
-app.set("views", path("./views"));
+app.set("views", path.resolve("./views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
