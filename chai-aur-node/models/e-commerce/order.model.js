98b8fdba9540ec5f79054b34b2c16d5639b1a279
shashi5657 <sangepushashidhar@gmail.com>
2025-12-08 14:30:29 +0530

    other data schemas for practicing

---
+++ chai-aur-node/models/e-commerce/order.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: [1, "Quantity must be at least 1"],
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    orderItems: [orderItemSchema],
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model("Order", orderSchema);
